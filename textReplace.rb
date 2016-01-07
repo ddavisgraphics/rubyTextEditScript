19452a31e597a64e5255940cc2b99173326a7962
David J. Davis <djdavis@mail.wvu.edu>
2016-01-07 12:01:20 -0500

    Scope of the work changed.

--- textReplace.rb
+++ textReplace.rb
@@ -1,127 +1,107 @@
-# Completed Solution uses CSV 
-# ==========================================
 require 'csv'
+
+# Get File
+# ==========================================
 input_string = File.read('./data/test.csv').force_encoding('MacRoman').encode('UTF-8')
-data = CSV.parse(input_string, :col_sep => ',')
+
+# create a better array for picking out data
+csv_file = CSV.new(input_string, :headers => true, :header_converters => :symbol)
+data = csv_file.to_a.map{ |row| row.to_hash }
 
 # create variable for new data to go into
-cleaned_data = []
-output       = ""
+cleaned_data    = []
+sm_patron_notes = []
+lg_patron_notes = []
+output          = ""
 
-data.each do |item|
+data.each do |row|
     # checks to make sure everything parsed correctly
-    if item.count > 38 || item.count < 38
-        puts "item count is off #{item.count}"
+    if row.count > 38 || row.count < 38
+        puts "item count is off #{row.count}"
     end
 
     #loop through the data rows and preform subs
-    changed_data = []
-    item.each do |row|
-        if row != nil
-            row = row.gsub(/\t/ , '')
-            row = row.gsub(/\r/ , '')
-            row = row.gsub(/\n/ , '')
-            row = row.gsub(/\s+/, ' ')
+    #put the data in a new hash so that we can pull into 2 types of array later
+    changed_data = Hash.new
+    row.each do |key, value|
+        if value != nil
+            value = value.gsub(/\t/ , '')
+            value = value.gsub(/\r/ , '')
+            value = value.gsub(/\n/ , '')
+            value = value.gsub(/\s+/, ' ')
         end
-        changed_data << row
+        changed_data[key] = value
     end
+    # send it to cleaned data
     cleaned_data << changed_data
 end
 
+#seperate the data into two seperate arrays based on a certain value
+cleaned_data.each do |row|
+    if row[:patronnotes] != nil
+        if row[:patronnotes].length < 255
+            row[:prefix] = row[:patronnotes].length
+            sm_patron_notes << row
+        else
+            row[:prefix] = row[:patronnotes].length;
+            lg_patron_notes << row
+        end
+    else
+        row[:prefix] = 0;
+        sm_patron_notes << row
+    end
+end
+
+# Create pipe Delimited File for array with less than 255 chars
+# =============================================================
 
 # create a new file and put the content in there
-output_file = File.new("fixedFile.txt", "w")
+output_file = File.new("fileLess255.txt", "w")
+
+#create the first line of headers
+first_line  = sm_patron_notes.first.keys
+first_line  = first_line.join('|')
+output_file.puts(first_line)
 
 # loop through the array again to break it into strings for a new line
-cleaned_data.each do |line|
-    output = line.join('|')
+sm_patron_notes.each do |line|
+    # get just the values not the keys
+    simple_array = line.values
+
+    # join the values in pipe delimited
+    output = simple_array.join('|')
     output += "\n"
+
+    # write it to the file
     output_file.puts(output)
 end
 
 #close the file and finish up
 output_file.close
+
+# Create pipe Delimted File for array with more than 255 chars
+# =============================================================
+
+# create a new file and put the content in there
+second_file = File.new("fileMore255.txt", "w")
+
+# create headers
+second_file.puts(first_line)
+
+# loop through the array again to break it into strings for a new line
+lg_patron_notes.each do |line|
+    # get just the values not the keys
+    simple_array = line.values
+
+    # join the values in pipe delimited
+    output = simple_array.join('|')
+    output += "\n"
+
+    # write it to the file
+    second_file.puts(output)
+end
+
+#close the file and finish up
+second_file.close
+
 puts "complete"
-
-
-## Initial File when working with TXT File
-# ===================================================
-
-# File.open('./data/original.txt') do |file|
-
-#     #declare needed vars
-#     output = ""
-#     groups = []
-#     line   = 0
-
-#     # temp array
-#     temp_array = [];
-
-#     # get number of records from the first line
-#     number_of_records = file.first.split('|').count
-
-#     file.each_line do |record|
-#         count_line = record.split('|').count
-#         if count_line < number_of_records
-#             # do nothing
-#         elsif count_line > number_of_records
-#             puts "LineNumber - #{line} : numRecords = #{count_line} "
-#         else
-#            # do nothing
-#         end
-
-#         line += 1
-#     end
-
-# end
-
-    # remove any spacing tabs or new lines from the entire document
-    # file.each_line do |record|
-
-    #     # count this line
-    #     count_line = record.split('|').count
-
-    #     # remove tabs spacings and returns
-    #     record = record.gsub(/\s+/, ' ')
-    #     record = record.gsub(/\t/ , '')
-    #     record = record.gsub(/\r/ , '')
-    #     record = record.gsub(/\n/ , '')
-
-    #     if count_line < number_of_records
-    #         if temp_array.count < number_of_records
-    #             temp_array << record.split('|')
-    #         elsif temp_array.count > number_of_records
-    #             temp_record = record.split('|')
-    #             groups << temp_record.slice!(0, number_of_records)
-    #             temp_array << temp_record
-    #         else
-    #             groups << record.split('|')
-    #         end
-    #     elsif count_line > number_of_records
-    #         puts count_line
-    #     else
-    #         groups << record.split('|')
-    #     end
-    # end
-
-    #puts groups.inspect
-
-   # parts = temp_string.split('|')
-   # puts parts.inspect
-
-    # # loop through the array and iterate it into smaller groups
-    # while(parts.any?)
-    #     groups << parts.slice!(0,38)
-    # end
-
-    # loop through the array again to break it into strings for a new line
-    # groups.each do |group|
-    #     output += group.join('|')
-    #     output += "\n"
-    # end
-
-    # # create a new file and put the content in there
-    # output_file = File.new("fixedFile.txt", "w")
-    # output_file.puts(output)
-    # output_file.close
-# end
